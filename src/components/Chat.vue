cb7974065f3b07f5e822601176a36615b5c5ab21
Shalaw Fatah <shalaw.fatah@gmail.com>
2020-07-27 16:32:44 -0700

    project almost finished

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -3,11 +3,11 @@
         <h2 class="center teal-text">DevArt Chat</h2>
         <div class="card">
             <div class="card-content">
-                <ul class="messages">
-                    <li>
-                        <span class="teal-text">Name</span>
-                        <span class="grey-text text-darken-3">message</span>
-                        <span class="grey-text time">time</span>
+                <ul class="messages" v-chat-scroll>
+                    <li v-for="message in messages" :key="message.id">
+                        <span class="teal-text">{{message.name}}</span>
+                        <span class="grey-text text-darken-3">{{message.content}}</span>
+                        <span class="grey-text time">{{message.timestamp}}</span>
 
                     </li>
                 </ul>
@@ -21,6 +21,9 @@
 
 <script>
 import NewMessage from '@/components/NewMessage'
+import db from '@/firebase/init'
+import moment from 'moment'
+
 export default {
     name: 'Chat',
     props: ['name'],
@@ -29,8 +32,24 @@ export default {
     },
     data() {
         return {
-
+            messages: []
         }
+    },
+    created() {
+        let ref = db.collection('messages').orderBy('timestamp')
+        ref.onSnapshot(snapshot => {
+            snapshot.docChanges().forEach((change)=> {
+                if(change.type === 'added') {
+                    let doc = change.doc
+                    this.messages.push({
+                        id: doc.id,
+                        name: doc.data().name,
+                        content: doc.data().content,
+                        timestamp: moment(doc.data().timestamp).format('llll')
+                    })
+                }
+            })
+        })
     }
 }
 </script>
@@ -45,6 +64,19 @@ export default {
 }
 .chat .time {
     display: block;
-    font-size: 1.2em;
+    font-size: 0.8em;
+}
+.messages {
+    max-height: 300px;
+    overflow: auto;
+}
+.messages::-webkit-scrollbar {
+    width: 3px;
+}
+.messages::-webkit-scrollbar-track {
+    background: #ddd;
+}
+.messages::-webkit-scrollbar-thumb {
+    background: #aaa;
 }
 </style>
