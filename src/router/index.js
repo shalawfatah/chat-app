fd391b27bb6359728f6339b3993b775406022214
Shalaw Fatah <shalaw.fatah@gmail.com>
2020-07-27 15:44:24 -0700

    added Chat and Chat windows

---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Chat from '@/components/Chat.vue'
+import Welcome from '@/components/Welcome.vue'
+
+Vue.use(Router)
+
+export default new Router ({
+  routes: [
+    {
+      path: '/',
+      name: 'Welcome',
+      component: Welcome
+    },
+    {
+      path: '/chat',
+      name: 'Chat',
+      component: Chat,
+      props: true,
+      beforeEnter: (to, from, next)=> {
+          if(to.params.name) {
+              next()
+          } else {
+              next({name: 'Welcome' })
+          }
+      } 
+    }
+  ]
+})
